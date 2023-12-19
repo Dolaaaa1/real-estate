a2192f9db471ccd3aa9d0451a917dbce04eac436
Ahmed Adel <119899462+Dolaaaa1@users.noreply.github.com>
2023-12-19 19:14:52 +0200

    image

--- src/listings/forms.py
+++ src/listings/forms.py
@@ -5,4 +5,4 @@ from .models import Listing
 class ListingForm(ModelForm):
     class Meta :
         model = Listing  
-        fields = ['title', 'price','num_bathroom','num_bedroom','square_footage','address',]
+        fields = ['title', 'price','num_bathroom','num_bedroom','square_footage','address','image',]
