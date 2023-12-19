f5cc4f85b1672408dcccd60e127261006d16ba1d
Ahmed Adel <119899462+Dolaaaa1@users.noreply.github.com>
2023-12-19 16:51:16 +0200

    tailwind css

--- src/project/urls.py
+++ src/project/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('',listing_list),
     path('listings/<pk>/',listing_retrieve),
     path('add-listing/',listing_create),
-    path('listings/<pk>/edit',listing_update),
-    path('listings/<pk>/delete',listing_delete)
+    path('listings/<pk>/edit/',listing_update),
+    path('listings/<pk>/delete/',listing_delete)
 ]
